3ea19b9070833d6ef4f6272a72e9939ecea22d1a
fronz <z233@msn.com>
2022-05-18 10:07:52 +0800

    feat: 3D parallax hover effects

--- views/index/Section1.vue
+++ views/index/Section1.vue
@@ -1,13 +1,46 @@
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
+import 'atropos/css'
+import Atropos from 'atropos/vue'
 </script>
 
-
 <template>
-  <div class="h-full text-primary bg-[url('assets/svg/background.svg')] bg-no-repeat 
-    bg-cover bg-center grid place-content-center text-center">
-    <h1 class="text-3xl font-bold">和我们一起，探索这个世界</h1>
-    <p class="text-lg mt-5">
-      滚滚长江东逝水，浪花淘尽英雄。是非成败转头空，青山依旧在，几度夕阳红。
-    </p>
-  </div>
+  <Atropos
+    :always-visible="true"
+    :shadow-scale="1.15"
+    :rotate-x-max="0.5"
+    :rotate-y-max="0.5"
+    class="h-full text-center relative"
+  >
+    <div
+      data-atropos-offset="-2"
+      class="bg bg-[url('assets/bg/mountain/sky.svg')]"
+    />
+    <div
+      data-atropos-offset="-1"
+      class="bg bg-[url('assets/bg/mountain/mountain.svg')]"
+    />
+    <div
+      data-atropos-offset="0"
+      class="bg bg-[url('assets/bg/mountain/hill.svg')]"
+    />
+    <div
+      data-atropos-offset="2"
+      class="bg bg-[url('assets/bg/mountain/foreground.svg')]"
+    />
+    <div class="absolute text-primary inset-0 grid place-content-center">
+      <h1 class="text-3xl font-bold">和我们一起，探索这个世界</h1>
+      <p class="text-lg mt-5">
+        滚滚长江东逝水，浪花淘尽英雄。是非成败转头空，青山依旧在，几度夕阳红。
+      </p>
+    </div>
+  </Atropos>
 </template>
+
+<style lang="postcss">
+.bg {
+  @apply absolute inset-0 h-full bg-no-repeat bg-cover bg-center;
+}
+</style>
