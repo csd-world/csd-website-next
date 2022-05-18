448da77a165037cede44b4a556361079c42e8d82
fronz <z233@msn.com>
2022-05-18 21:40:52 +0800

    feat: drop down hint

--- views/index/Section1.vue
+++ views/index/Section1.vue
@@ -43,6 +43,14 @@ import Atropos from 'atropos/vue'
           >马上加入</BaseButton
         >
       </div>
+      <div
+        class="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-sm"
+      >
+        <div class="animate-bounce flex flex-col items-center">
+          <div class="mb-1">下拉了解更多</div>
+          <span class="gg-chevron-down"></span>
+        </div>
+      </div>
     </div>
   </Atropos>
 </template>
@@ -50,5 +58,32 @@ import Atropos from 'atropos/vue'
 <style lang="postcss">
 .bg {
   @apply absolute inset-0 h-full bg-no-repeat bg-cover bg-center;
+}
+
+/* ICONS */
+
+.gg-chevron-down {
+  box-sizing: border-box;
+  position: relative;
+  display: inline-block;
+  transform: scale(var(--ggs, 1));
+  width: 22px;
+  height: 22px;
+  border: 2px solid transparent;
+  border-radius: 100px;
+}
+
+.gg-chevron-down::after {
+  content: '';
+  display: block;
+  box-sizing: border-box;
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  border-bottom: 2px solid;
+  border-right: 2px solid;
+  transform: rotate(45deg);
+  left: 4px;
+  top: 2px;
 }
 </style>
