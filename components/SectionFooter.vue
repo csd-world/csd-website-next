ed8af605ee027fba9c60888b774fc9ff0595a93e
lorde <rioaformyking@gmail.com>
2022-09-13 22:21:21 +0800

    add: sectionFooter (doge

--- components/SectionFooter.vue
+++ components/SectionFooter.vue
@@ -1,5 +1,8 @@
 <template>
   <p class="absolute bottom-2 left-1/2 -translate-x-1/2 text-shadow-md opacity-70 w-auto text-sm whitespace-nowrap">
-    © 2022 Designed & Coded with <span class="iconfont icon-love mx-1"></span> by <a href="http://github.com/z233" target="_blank" class="hover:opacity-100">Fronz</a>
+    © 2022 Designed & Coded with <span class="iconfont icon-love mx-1"></span> by <a href="http://github.com/z233"
+      target="_blank" class="hover:opacity-100">Fronz</a> &
+      <a href="https://github.com/Lorde4Avalon"
+      target="_blank" class="hover:opacity-100">Lorde</a>
   </p>
 </template>
