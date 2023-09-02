a3dfdeab4c278f5340e5b77a590d2c369944f101
z233 <z233@msn.com>
2023-09-02 18:34:23 +0800

    chore: update section footer info

--- components/SectionFooter.vue
+++ components/SectionFooter.vue
@@ -1,8 +1,6 @@
 <template>
   <p class="absolute bottom-2 left-1/2 -translate-x-1/2 text-shadow-md opacity-70 w-auto text-sm whitespace-nowrap">
-    © 2022 Designed & Coded with <span class="iconfont icon-love mx-1"></span> by <a href="http://github.com/z233"
-      target="_blank" class="hover:opacity-100">Fronz</a> &
-      <a href="https://github.com/Lorde4Avalon"
-      target="_blank" class="hover:opacity-100">Lorde</a>
+    © 2023 Designed & Coded with <span class="iconfont icon-love mx-1"></span> by <a href="https://github.com/csd-world/csd-website-next"
+      target="_blank" class="hover:opacity-100">软件部</a>
   </p>
 </template>
